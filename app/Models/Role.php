aa5143dfc930e8eb726715e0dcd90a36e25b0dfd
KalypsoDev <antunezangela89@gmail.com>
2024-03-24 15:07:35 +0100

    Establish the relationship beetween Role and User

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -9,4 +9,9 @@ class Role extends Model
 {
     use HasFactory;
     protected $guarded = []; //indicamos que campos de la tabla no se puede manipular a traves del modelo con guarded
+
+    public function user()
+    {
+        return $this->hasMany(User::class);
+    }
 }
